19e74d7eeeb3bd9998c951d8ab65badb5797c2d8
anargasimzada <tu7g11auv@code.edu.az>
2024-04-03 03:28:48 +0400

    access mod project

---
+++ AccessModifiers02.04/Moduls/Weapon.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers02._04.Moduls
+{
+    internal class Weapon
+    {
+        public int _bulletCapacity;
+        public int _numberOfBullet;
+        public int _bulDischargeSec;
+        public bool ShootMode=false;
+
+        public  int BulletCapacity
+        {
+            get
+            {
+                return _bulletCapacity;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCapacity = value;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("daxil edilen reqem yanlisdir.Yeniden daxil edin");
+                    
+                }
+            }
+        }
+        public int NumberOfBullet
+        {
+            get
+            {
+                return _numberOfBullet;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _numberOfBullet = value;
+                }
+               
+            }
+        }
+        public int BulDischargeSec
+        {
+            get
+            {
+                return _bulDischargeSec;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulDischargeSec = value;
+                }
+            }
+        }
+        public Weapon(int bulletcapacity,int numberofbullet)
+        {
+            BulletCapacity = bulletcapacity;
+            NumberOfBullet=numberofbullet;
+           
+        }
+        public void Shoot()
+        {
+            if (!ShootMode)
+            {
+                if (NumberOfBullet!=0)
+                {
+                    _numberOfBullet--;
+                    Console.WriteLine($"ates acildi,daragda gulle sayi{_numberOfBullet}");
+                }
+                else
+                {
+                    Console.WriteLine("darag bosdur");
+                }
+            }
+        }
+        public void Fire()
+        {
+          
+            if (!ShootMode)
+            {
+                while (NumberOfBullet != 0)
+                {
+                    _numberOfBullet--;
+                    BulDischargeSec++;
+                }
+               
+                //herbir gullenin silahdan cixma muddetini 1 saniye gotururem
+                Console.WriteLine($"Gulleler ateslendikden {BulDischargeSec} saniye sonra bitir");
+
+               
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            int count=0;
+            count = BulletCapacity-NumberOfBullet;
+            Console.WriteLine(count);
+
+        }
+        public void Reload()
+        {
+            int save = 0;
+            save = BulletCapacity - NumberOfBullet;
+            BulletCapacity=save+NumberOfBullet;
+            Console.WriteLine(BulletCapacity);
+        }
+        public bool ChangeFireMood()
+        {
+            if (ShootMode)
+            {
+                Shoot();
+                Console.WriteLine("tekli mood");
+                return false;
+            }
+            else
+            {
+                Fire();
+                Console.WriteLine("avtomatik mood");
+                return true;
+            }
+        }
+    }
+}
---
+++ AccessModifiers02.04/Program.cs
@@ -0,0 +1,73 @@
+﻿using AccessModifiers02._04.Moduls;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace AccessModifiers02._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(20,15);
+            //Console.WriteLine(weapon.BulletCapacity);
+          //  weapon.Fire();
+          // weapon.Shoot();
+          //weapon.GetRemainBulletCount();
+          //  weapon.Reload();
+                      int num;
+            do
+            {
+                Console.WriteLine(@"0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 - GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandırmaq üçün
+7 - Redaktə et :
+T - Tutumu dəyişsin
+S - Güllə sayı
+D - Sıfırlama saniyəsi");
+                if (int.TryParse(Console.ReadLine(), out num))
+                {
+                    switch (num)
+                    {
+                        //case 0:
+                        //    weapon.GetInfo();
+                        //    break;
+                        case 1:
+                            weapon.Shoot();
+                            break;
+                        case 2:
+                            weapon.Fire();
+                            break;
+                        case 3:
+                            weapon.GetRemainBulletCount();
+                            break;
+                        case 4:
+                                weapon.Reload();
+                            break;
+                            case 5:
+                                weapon.ChangeFireMood();
+                            break;
+                        case 6:
+                            Console.WriteLine("Çıxış...");
+                            break;
+                        default:
+                            Console.WriteLine("Başqa rəqəm daxil edin.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Reqem daxil edin.");
+                }
+
+                Console.WriteLine();
+            } while (num != 0);
+
+
+        }
+        
+    }
+}
